152bf78a74e5787a078ca805589b234463784dfe
virinchiv <virinchi0407@gmail.com>
2025-06-24 19:55:28 -0700

    Finished Profile screen for user
    Display's users details, allows to edit profile, change password, prompts to additional settings, and displays logout option.

--- App.js
+++ App.js
@@ -5,16 +5,20 @@ import Auth from './Auth';
 import Login from './Login';
 import Signup from './Signup';
 import OTPVerification from './OTPVerification';
+import Profile from './profile';
+import Signup2 from './SignupProfile';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="Auth" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="Auth" component={Auth} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Signup2" component={Signup2} />
         <Stack.Screen name="OTPVerification" component={OTPVerification} />
       </Stack.Navigator>
     </NavigationContainer>
